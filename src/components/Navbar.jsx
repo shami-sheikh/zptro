2855c5be5cf0063edc190bd106f5b864efeeb726
shami-sheikh <your-github-samidev.co@gmail.com>
2026-02-21 10:14:59 +0530

    solve mobile icon problem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const {cartitems}=useContext(CartContext)
               <Link to="/cart" className="relative">
                 <FaCartShopping className="text-red-500 h-6 w-6 cursor-pointer" />
                 <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
-                  0
+                 {cartitems.length}
                 </span>
               </Link>
 
